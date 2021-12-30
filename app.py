c694d5e73bc6ac80057345e9190c79c77f4e50d9
nplaybook <naufal.abdila@outlook.com>
2021-12-30 15:25:29 +0700

    add initial socket runner

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret!"
+socketio = SocketIO(app)
 
-@app.get("/")
-def index():
-    return "Hello world"
+@socketio.on("message")
+def handle_message(data):
+    print(f"Received message: {data}")
+
+@socketio.on("json")
+def handle_json(json):
+    print(f"Received message: {json}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app)
