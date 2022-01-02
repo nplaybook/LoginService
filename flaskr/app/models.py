18b8a1454d180b7b5ced277754ac4659a4d48b25
nplaybook <naufal.abdila@outlook.com>
2022-01-02 17:35:22 +0700

    update model

--- flaskr/app/models.py
+++ flaskr/app/models.py
@@ -7,10 +7,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True)
     username = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
-    created_at = db.Column(db.DateTime, default=datetime.now)
+    salt = db.Column(db.String, nullable=False)
+    hash = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
 
-    def __init__(self, email: str, username: str, password: str) -> None:
+    def __init__(self, email: str, username: str, salt: str, hash: str) -> None:
         self.email = email
         self.username = username
-        self.password = password
+        self.salt = salt
+        self.hash = hash
