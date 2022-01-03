1eeeffbbfbf63527e25922d00260a5f3df3ebd39
nplaybook <naufal.abdila@outlook.com>
2022-01-03 23:57:18 +0700

    specifiy templates and static location

--- flaskr/app/__init__.py
+++ flaskr/app/__init__.py
@@ -7,7 +7,7 @@ def create_app(db: SQLAlchemy) -> Flask:
     from .routes.views import view
     from .routes.route import api
 
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="./routes/templates", static_folder="./routes/static")
     app.config['SECRET_KEY'] = 'mysecret'
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
     app.config["SQLALCHEMY_TRACK_MODIFICATNIOS"] = False
