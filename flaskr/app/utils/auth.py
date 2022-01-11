1da7949f12453b111a5b0421a3929a985ba0dd37
nplaybook <naufal.abdila@outlook.com>
2022-01-11 22:52:54 +0700

    simplify check user function, add more blankspaces to improve readability, create decorator function to check authenticated user

--- flaskr/app/utils/auth.py
+++ flaskr/app/utils/auth.py
@@ -1,47 +1,35 @@
 import os
-import jwt
 from datetime import datetime, timedelta
 from hashlib import pbkdf2_hmac
-from flask import current_app
+from functools import wraps
+
+import jwt
+from flask import current_app, request, session, jsonify
+
 from flaskr.app.models.user import User
+from flaskr.app.utils.response import generate_response
 
-def check_user_login(email: str) -> bool: 
-    """Check existing user login info from input usename and email.
+
+def check_existing_user(email: str, username: str=None):
+    """Check existing user info from input email and usename (optional).
     If user already exist will return True, otherwise False.
     
     :param email: {str} input email from request body
-    
-    :return: {bool}
-    """
-
-    validation: bool = False
-
-    validate_email = User.query.filter_by(email=email).first()
-    validation = True if validate_email is None else False
- 
-    return validation
-
-def check_user_signup(email: str, username: str) -> bool: 
-    """Check existing user signup info from input usename and email.
-    If user already exist will return True, otherwise False.
-    
     :param username: {str} input username from request body
-    :param email: {str} input email from request body
-    
     :return: {bool}
     """
 
     validation: bool = True
     
-    validate_username = User.query.filter_by(username=username).first()
-    validation = True if validate_username is not None else False
-    if validation: return validation
-    
-
     validate_email = User.query.filter_by(email=email).first()
     validation = True if validate_email is not None else False
- 
+    
+    if username:
+        validate_username = User.query.filter_by(username=username).first()
+        validation = True if validate_username is not None else False
+
     return validation
+
 
 def get_user_data(email: str) -> dict:
     """Query user data by corresponding email.
@@ -52,6 +40,7 @@ def get_user_data(email: str) -> dict:
     
     user_data = User.query.filter_by(email=email).first()
     return user_data
+
 
 def mask_password(password: str) -> tuple:
     """Mask password to database as salt and hash.
@@ -69,6 +58,7 @@ def mask_password(password: str) -> tuple:
         iterations=100_000
     )
     return salt, hash
+
 
 def match_password(password: str, salt: str, hash: str) -> bool:
     """Match password from request body and salt-hash pair
@@ -91,6 +81,7 @@ def match_password(password: str, salt: str, hash: str) -> bool:
 
     return True if generated_hash == hash else False
 
+
 def issue_jwt(data: dict, expired_minutes: int) -> dict:
     """Create JWT for authentication purpose.
     
@@ -102,7 +93,37 @@ def issue_jwt(data: dict, expired_minutes: int) -> dict:
     """
 
     token = jwt.encode({
-        "user": data.username, 
+        "username": data.username,
+        "email": data.email,
         "exp": datetime.utcnow() + timedelta(minutes=expired_minutes)
     }, key=current_app.config["SECRET_KEY"])
     return token
+
+
+def token_required(func):
+    """Decorator that will ensure the visiting route
+    can be accessed only by authenticated user
+    """
+
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = None
+        
+        if "x-access-token" in request.headers:
+            token = request.headers["x-access-token"]
+        elif "token" in session:
+            token = session["token"]
+        
+        if not token:
+            response = generate_response(http_status=401, message="Token is missing")
+            return jsonify(response), 401
+
+        try:
+            data = jwt.decode(token, key=current_app.config["SECRET_KEY"])
+            user = User.query.filter_by(username=data["username"]).first()
+        except:
+            response = generate_response(http_status=401, message="Token is invalid")
+            return jsonify(response), 401
+
+        return func(user, *args, **kwargs)
+    return decorated
