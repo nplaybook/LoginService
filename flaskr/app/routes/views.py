8c883639a9050a84e54e9e1531b9b9a5759e63d3
nplaybook <naufal.abdila@outlook.com>
2022-01-11 22:54:09 +0700

    delete unused route

--- flaskr/app/routes/views.py
+++ flaskr/app/routes/views.py
@@ -2,10 +2,6 @@ from flask import Blueprint, render_template
 
 view = Blueprint("views", __name__)
 
-@view.get("/")
-def index():
-    return "Index"
-
 @view.get("/login")
 def login():
     return render_template("login.html")
