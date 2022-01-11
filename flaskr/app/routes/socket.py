c096f0b87e2b7ebd73db74f785457fe77f12aea9
nplaybook <naufal.abdila@outlook.com>
2022-01-11 22:56:34 +0700

    remove unused file

--- flaskr/app/routes/socket.py
+++
@@ -1,7 +0,0 @@
-# from flaskr import socketio
-# from flask_socketio import send
-
-# @socketio.on('message')
-# def handle_message(message):
-# 	print('Message: ' + message)
-# 	send(message, broadcast=True)
