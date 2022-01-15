520479ecd7e7267ea5c73161066b291a2835830e
nplaybook <naufal.abdila@outlook.com>
2022-01-15 10:00:23 +0700

    remove socketio related, separate other service from login

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,15 +1,8 @@
-from flask_socketio import SocketIO, send
 from flask_sqlalchemy import SQLAlchemy
 from flaskr.app import create_app
 
 db = SQLAlchemy()
 app = create_app(db)
-socketio = SocketIO(app, cors_allowed_origins='*')
-
-@socketio.on('message')
-def handle_message(message):
-    print('Message: ' + message)
-    send(message, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run()
--- flaskr/app/__init__.py
+++ flaskr/app/__init__.py
@@ -5,18 +5,16 @@ from flask_sqlalchemy import SQLAlchemy
 def create_app(db: SQLAlchemy) -> Flask:
     """Main caller to create Flask application"""
 
-    from .routes.views import view
     from .routes.route import api
 
-    app = Flask(__name__, template_folder="./routes/templates", static_folder="./routes/static")
+    app = Flask(__name__)
     app.config['SECRET_KEY'] = 'mysecret'
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-    app.config["SQLALCHEMY_TRACK_MODIFICATNIOS"] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     CORS(app)
     
     db.init_app(app)
 
-    app.register_blueprint(view, url_prefix="/")
     app.register_blueprint(api, url_prefix="/api/")
     
     app.debug=True
