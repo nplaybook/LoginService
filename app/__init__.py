e1253a3a3c4dc53651f7b6938fb1267845ca1384
nplaybook <naufal.abdila@outlook.com>
2022-01-15 16:45:15 +0700

    unwrap flask app from function

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,19 @@
 from flask import Flask
 from flask_cors import CORS
+
 from app.ext import db
+from app.routes.route import api
 
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'mysecret'
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    register_extension(app)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    CORS(app)
+CORS(app)
+db.init_app(app)
 
-    from app.routes.route import api
-    app.register_blueprint(api)
+app.register_blueprint(api)
 
-    return app
-
-
-def register_extension(app):
-    db.init_app(app)
+if __name__ == "__main__":
+    app.run()
