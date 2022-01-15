d110146aef2ef4240b6b74f4bd46990735ba243f
nplaybook <naufal.abdila@outlook.com>
2022-01-15 21:20:31 +0700

    change email validator to Email (check the data structure)

--- app/utils/validator.py
+++ app/utils/validator.py
@@ -1,15 +1,15 @@
 from marshmallow import Schema, fields
 
 class UserRegister(Schema):
-    email = fields.String(required=True)
+    email = fields.Email(required=True)
     username = fields.String(required=True)
     password = fields.String(required=True)
 
 class UserLogin(Schema):
-    email = fields.String(required=True)
+    email = fields.Email(required=True)
     password = fields.String(required=True)
 
 class UserResetPassword(Schema):
-    email = fields.String(required=True)
+    email = fields.Email(required=True)
     username = fields.String(required=True)
     password = fields.String(required=True)
